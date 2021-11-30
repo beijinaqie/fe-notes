560ba25bab840e2cac0cb905ab906e19df1b24cf
beijinaqie <m13273794916@163.com>
2021-11-30 17:20:36 +0800

    优化ejs

--- Node/ejs/index.js
+++ Node/ejs/index.js
@@ -9,22 +9,20 @@ let data = {
 
 let ejs = {
   render(html, data) {
+    let header = 'with(data) { \nlet str = ""; \nstr+=`'
+    let footer = '`\nreturn str}'
+    html = html.replace(/<%=(.+?)%>/g, (match, $1) => {
+      return '${' + $1 + '}';
+    })
+    html = html.replace(/<%(.+?)%>/g, (match, $1) => {
+      return '`\n' + $1 + '\nstr += `';
+    })
+
+    html = header + html + footer
     return new Function('data', html)(data)
   }
 }
 
 let html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8')
-
-let header = 'with(data) { \nlet str = ""; \nstr+=`'
-let footer = '`\nreturn str}'
-html = html.replace(/<%=(.+?)%>/g, (match, $1) => {
-  return '${' + $1 + '}';
-})
-html = html.replace(/<%(.+?)%>/g, (match, $1) => {
-  return '`\n' + $1 + '\nstr += `';
-})
-
-html = header + html + footer
-
 let r = ejs.render(html, data)
 console.log(r);
