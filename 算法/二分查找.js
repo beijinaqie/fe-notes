95d05fa3884b303883bbd1c99ee85be1baf78a2f
beijinaqie <m13273794916@163.com>
2021-12-02 16:20:20 +0800

    新增边界断言

--- 算法/二分查找.js
+++ 算法/二分查找.js
@@ -1,36 +1,37 @@
 const assert = require('assert');
 
 let arr = [3, 5, 19, 22, 25, 33, 45, 47, 57, 66, 71, 78, 81]
+let arr1 = []
+let arr2 = [2]
 
 function binarySearch(arr, target) {
-  if (!arr.length) return
-  if (arr.length === 1) return arr[0]
+  let l = 0, 
+      r = arr.length - 1,
+      g
 
-  let l = 0;
-  let r = arr.length - 1;
-  let g = ~~(r / 2)
-
-  function recursive() {
+  while (l <= r) {
+    g = ~~((l + r) / 2)
     // console.log(l, r, g, arr[g]);
-    if (l > r || r < l) return -1
-    if (arr[g] === target) return g;
-
-    if (arr[g] < target) {
-      l = g + 1;
-      g = ~~((r + l) / 2)
+    if (arr[g] === target) {
+      return g
+    } else if (arr[g] > target) {
+      r = g - 1
     } else {
-      r = g - 1;
-      g = ~~((r - l) / 2)
+      l = g + 1
     }
-    return recursive()
   }
-  return recursive();
+  return -1
 }
-
 
 // 断言测试
 assert.strictEqual(binarySearch(arr, 1), -1)
+assert.strictEqual(binarySearch(arr, 100), -1)
 assert.strictEqual(binarySearch(arr, 19), 2)
 assert.strictEqual(binarySearch(arr, 66), 9)
 assert.strictEqual(binarySearch(arr, 3), 0)
 assert.strictEqual(binarySearch(arr, 81), 12)
+assert.strictEqual(binarySearch(arr1, 1), -1)
+assert.strictEqual(binarySearch(arr2, 1), -1)
+assert.strictEqual(binarySearch(arr2, 2), 0)
+console.log('断言通过');
+
